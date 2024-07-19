a9749ab6250169249de8e231ef238613c41738a2
Ram Kinkar <rkgarg15042005@gmail.com>
2024-07-19 17:32:06 +0530

    day2.js added

---
+++ day1.js
@@ -0,0 +1,18 @@
+var a =45;
+console.log(a);
+
+let b="ram ";
+console.log(b);
+
+const c=true;
+console.log(c);
+
+const d=[1,2,3];
+
+const e={"name":"ram", "branch":"CSE" , "roll no:":123102127}
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
